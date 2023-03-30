2ff0f3d691380dd5f65f7e660eec4c141bc9cf1f
SadiqAgayev <sadiqagayev820@gmail.com>
2023-03-30 14:17:18 +0400

    initialize project

---
+++ components/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header>
+        {/* Left */}
+        <div>
+            
+        </div>
+
+        {/* Middle */}
+        <div></div>
+
+        {/* Right */}
+        <div></div>
+    </header>
+  )
+}
+
+export default Header
